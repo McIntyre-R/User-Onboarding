1e8eba6f07eda7ffd410652df0ee2d1cc53c8dc4
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-02-27 20:15:45 -0500

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import FormikOnboard from './components/Form'
 
 function App() {
   return (
     <div className="App">
-     
+      <FormikOnboard />
     </div>
   );
 }
